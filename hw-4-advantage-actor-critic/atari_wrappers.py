322febdae249c010ec932b62b886dbfa4c405715
qbrick <qbrick@mail.ru>
2020-09-11 17:28:51 +0300

    A2C major update

--- hw-4-advantage-actor-critic/atari_wrappers.py
+++ hw-4-advantage-actor-critic/atari_wrappers.py
@@ -2,11 +2,13 @@
 from collections import deque
 
 import cv2
+import numpy as np
+
 import gym
 import gym.spaces as spaces
 from gym.envs import atari
-import numpy as np
-import tensorflow as tf
+from gym.wrappers import Monitor
+from tensorboardX import SummaryWriter
 
 from env_batch import ParallelEnvBatch
 cv2.ocl.setUseOpenCL(False)
@@ -204,24 +206,35 @@ class ClipReward(gym.RewardWrapper):
 
     def reward(self, reward):
         return np.sign(reward)
+    
+class ImageToPyTorch(gym.ObservationWrapper):
+    """
+    Image shape to num_channels x weight x height and normalization
+    """
+    def __init__(self, env):
+        super(ImageToPyTorch, self).__init__(env)
+        old_shape = self.observation_space.shape
+        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(old_shape[-1], old_shape[1], old_shape[0]), dtype=np.float32)
 
+    def observation(self, observation):
+        return np.swapaxes(observation, 2, 0).astype(np.float32) / 255.0
 
-class TFSummaries(gym.Wrapper):
+class TensorboardSummaries(gym.Wrapper):
     """ Writes env summaries."""
 
     def __init__(self, env, prefix=None, running_mean_size=100, step_var=None):
-        super(TFSummaries, self).__init__(env)
+        super(TensorboardSummaries, self).__init__(env)
         self.episode_counter = 0
         self.prefix = prefix or self.env.spec.id
-        self.step_var = (step_var if step_var is not None
-                         else tf.train.get_global_step())
+        self.writer = SummaryWriter(f"logs/{self.prefix}")
+        self.step_var = 0
 
-        nenvs = getattr(self.env.unwrapped, "nenvs", 1)
-        self.rewards = np.zeros(nenvs)
-        self.had_ended_episodes = np.zeros(nenvs, dtype=np.bool)
-        self.episode_lengths = np.zeros(nenvs)
+        self.nenvs = getattr(self.env.unwrapped, "nenvs", 1)
+        self.rewards = np.zeros(self.nenvs)
+        self.had_ended_episodes = np.zeros(self.nenvs, dtype=np.bool)
+        self.episode_lengths = np.zeros(self.nenvs)
         self.reward_queues = [deque([], maxlen=running_mean_size)
-                              for _ in range(nenvs)]
+                              for _ in range(self.nenvs)]
 
     def should_write_summaries(self):
         """ Returns true if it's time to write summaries. """
@@ -229,27 +242,32 @@ class TFSummaries(gym.Wrapper):
 
     def add_summaries(self):
         """ Writes summaries. """
-        tf.contrib.summary.scalar(
-            f"{self.prefix}/total_reward",
-            tf.reduce_mean([q[-1] for q in self.reward_queues]),
-            step=self.step_var)
-        tf.contrib.summary.scalar(
-            f"{self.prefix}/reward_mean_{self.reward_queues[0].maxlen}",
-            tf.reduce_mean([np.mean(q) for q in self.reward_queues]),
-            step=self.step_var)
-        tf.contrib.summary.scalar(
-            f"{self.prefix}/episode_length",
-            tf.reduce_mean(self.episode_lengths),
-            step=self.step_var)
+        self.writer.add_scalar(
+            f"Episodes/total_reward",
+            np.mean([q[-1] for q in self.reward_queues]),
+            self.step_var
+        )
+        self.writer.add_scalar(
+            f"Episodes/reward_mean_{self.reward_queues[0].maxlen}",
+            np.mean([np.mean(q) for q in self.reward_queues]),
+            self.step_var
+        )
+        self.writer.add_scalar(
+            f"Episodes/episode_length",
+            np.mean(self.episode_lengths),
+            self.step_var
+        )
         if self.had_ended_episodes.size > 1:
-            tf.contrib.summary.scalar(
-                f"{self.prefix}/min_reward",
+            self.writer.add_scalar(
+                f"Episodes/min_reward",
                 min(q[-1] for q in self.reward_queues),
-                step=self.step_var)
-            tf.contrib.summary.scalar(
-                f"{self.prefix}/max_reward",
+                self.step_var
+            )
+            self.writer.add_scalar(
+                f"Episodes/max_reward",
                 max(q[-1] for q in self.reward_queues),
-                step=self.step_var)
+                self.step_var
+            )
         self.episode_lengths.fill(0)
         self.had_ended_episodes.fill(False)
 
@@ -268,6 +286,7 @@ class TFSummaries(gym.Wrapper):
             self.reward_queues[i].append(self.rewards[i])
             self.rewards[i] = 0
 
+        self.step_var += self.nenvs
         if self.should_write_summaries():
             self.add_summaries()
         return obs, rew, done, info
@@ -278,10 +297,19 @@ class TFSummaries(gym.Wrapper):
         self.had_ended_episodes.fill(False)
         return self.env.reset(**kwargs)
 
+# magic for parallel launching of environments
+class _thunk:
+    def __init__(self, i, env_seed, env_id, **kwargs):
+        self.env_id = env_id
+        self.i = i
+        self.env_seed = env_seed
+        self.kwargs = kwargs
 
-def nature_dqn_env(env_id, nenvs=None, seed=None,
-                   summaries=True, clip_reward=True):
-    """ Wraps env as in Nature DQN paper. """
+    def __call__(self):
+        return nature_dqn_env(self.env_id, seed=self.env_seed, summaries=False, clip_reward=False, **self.kwargs)
+
+def nature_dqn_env(env_id, nenvs=None, seed=None, summaries=True, monitor=False, clip_reward=True, episodic_life=True):
+    """ Wraps env as in Nature DQN paper and creates parallel actors. """
     if "NoFrameskip" not in env_id:
         raise ValueError(f"env_id must have 'NoFrameskip' but is {env_id}")
     if nenvs is not None:
@@ -293,29 +321,36 @@ def nature_dqn_env(env_id, nenvs=None, seed=None,
             raise ValueError(f"seed has length {len(seed)} but must have "
                              f"length equal to nenvs which is {nenvs}")
 
-        env = ParallelEnvBatch([
-            lambda i=i, env_seed=env_seed: nature_dqn_env(
-                env_id, seed=env_seed, summaries=False, clip_reward=False)
-            for i, env_seed in enumerate(seed)
-        ])
+        thunks = [_thunk(i, env_seed, env_id, episodic_life=episodic_life) for i, env_seed in enumerate(seed)]
+        env = ParallelEnvBatch(thunks)
+
         if summaries:
-            env = TFSummaries(env, prefix=env_id)
+            env = TensorboardSummaries(env, prefix=env_id)
         if clip_reward:
             env = ClipReward(env)
         return env
 
     env = gym.make(env_id)
     env.seed(seed)
+    
     if summaries:
-        env = TFSummaries(env)
-    env = EpisodicLife(env)
+        env = TensorboardSummaries(env)
+    
     if "FIRE" in env.unwrapped.get_action_meanings():
         env = FireReset(env)
     env = StartWithRandomActions(env, max_random_actions=30)
+    
+    if monitor:
+        env = gym.wrappers.Monitor(env, directory="videos", force=True)
+    if episodic_life:
+        env = EpisodicLife(env)
+        
     env = MaxBetweenFrames(env)
     env = SkipFrames(env, 4)
     env = ImagePreprocessing(env, width=84, height=84, grayscale=True)
     env = QueueFrames(env, 4)
+    env = ImageToPyTorch(env)
     if clip_reward:
         env = ClipReward(env)
     return env
+
--- hw-4-advantage-actor-critic/runners.py
+++ hw-4-advantage-actor-critic/runners.py
@@ -13,16 +13,23 @@ class EnvRunner:
         self.nsteps = nsteps
         self.transforms = transforms or []
         self.step_var = step_var if step_var is not None else 0
-        self.state = {"latest_observation": self.env.reset()}
+        self.latest_observation = self.env.reset()
 
     @property
     def nenvs(self):
         """ Returns number of batched envs or `None` if env is not batched """
         return getattr(self.env.unwrapped, "nenvs", None)
+    
+    def write(self, name, val):
+        """ Writes logs """
+        if type(val) is dict:
+            self.env.writer.add_scalars(name, val, self.env.step_var)
+        else:
+            self.env.writer.add_scalar(name, val, self.env.step_var)
 
     def reset(self):
         """ Resets env and runner states. """
-        self.state["latest_observation"] = self.env.reset()
+        self.latest_observation = self.env.reset()
         self.policy.reset()
 
     def get_next(self):
@@ -30,36 +37,37 @@ class EnvRunner:
         trajectory = defaultdict(list, {"actions": []})
         observations = []
         rewards = []
-        resets = []
-        self.state["env_steps"] = self.nsteps
-
+        dones = []
+        
         for i in range(self.nsteps):
-            observations.append(self.state["latest_observation"])
-            act = self.policy.act(self.state["latest_observation"])
+            observations.append(self.latest_observation)
+            act = self.policy.act(self.latest_observation)
+            
             if "actions" not in act:
                 raise ValueError("result of policy.act must contain 'actions' "
                                  f"but has keys {list(act.keys())}")
+            assert type(act["actions"]) is np.ndarray, "Error: actions for environment must be numpy"
+                
             for key, val in act.items():
                 trajectory[key].append(val)
 
             obs, rew, done, _ = self.env.step(trajectory["actions"][-1])
-            self.state["latest_observation"] = obs
+            self.latest_observation = obs
             rewards.append(rew)
-            resets.append(done)
+            dones.append(done)
             self.step_var += self.nenvs or 1
 
             # Only reset if the env is not batched. Batched envs should
             # auto-reset.
             if not self.nenvs and np.all(done):
-                self.state["env_steps"] = i + 1
-                self.state["latest_observation"] = self.env.reset()
+                self.latest_observation = self.env.reset()
 
         trajectory.update(
             observations=observations,
             rewards=rewards,
-            resets=resets)
-        trajectory["state"] = self.state
+            dones=dones)
 
         for transform in self.transforms:
-            transform(trajectory)
+            transform(trajectory, self.latest_observation)
+        
         return trajectory
